d93c6f53e2723be652fea54bae107b92be59f93c
Dexter <bsse1021@iit.du.ac.bd>
2020-08-14 17:00:03 +0600

    Modified Jumbotron

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Jumbotron.css";
+import { Jumbotron as Jumbo, Container } from "react-bootstrap";
 
 export const Jumbotron = () => (
-  <div className="jumbotron">
+  <Jumbo>
     <div className="overly"></div>
-    <h1 className="display-4 container">Welcome</h1>
-    <p className="lead container">Here You Can Learn Code From Stratch!!!</p>
-  </div>
+    <Container>
+      <h1 className="display-4 ">Welcome</h1>
+      <p className="lead ">Here You Can Learn Code From Stratch!!!</p>
+    </Container>
+  </Jumbo>
 );
